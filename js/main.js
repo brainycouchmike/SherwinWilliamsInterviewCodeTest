33ddc3dd8063dd0999c561558f79af9ec6f3a527
Mike Simoneau <mike@brainycouch.com>
2015-04-03 12:30:46 -0400

    Update with Finished Product

---
+++ js/main.js
@@ -0,0 +1,39 @@
+(function(d) {
+  function App() {
+    function clearAndFocus() {
+      if(!!this.classList && !!this.classList.contains('active')) return false;
+      var title = this;
+      var activ = d.querySelectorAll(".accordian-title.active");
+      if(!!activ&&!!activ.length) for(var e in activ) {
+        if(!activ[e].hasOwnProperty('classList')) continue;
+        activ[e].classList.remove("active");
+        activ[e].nextElementSibling.classList.remove("active");
+      }
+      var detail = title.nextElementSibling;
+      if(!!detail&&!!detail.classList) {
+        title.classList.add("active");
+        detail.classList.add("active");
+      }
+    }
+    function initBindings() {
+      Array.prototype.slice.call(
+        // Create Array from NodeList (accordian titles)
+        d.querySelectorAll(".accordian-title")
+      ).forEach(function(title){
+        // Loop through Each and Attach Handle
+        title.addEventListener("click",clearAndFocus,false);
+      });
+    }
+    (function() {
+      initBindings();
+    })(); // INIT
+  }
+  var loaded = function() {
+    return(["interactive", "complete"].indexOf(document.readyState) !== -1);
+  };
+  var checkLoad = function() {
+    if(!loaded()) setTimeout(checkLoad, 100);
+    else App();
+  };
+  checkLoad();
+})(document);
